d93f5740de3b3aae8dab169693919e9543947033
Grzegorz Sztechmiler <sztechmi@d17-l427-pc-09.ipa.iisg.agh.edu.pl>
2019-04-28 15:13:49 +0200

    correct asserts

--- src/test/java/pl/edu/agh/mwo/calculator/CalcTest.java
+++ src/test/java/pl/edu/agh/mwo/calculator/CalcTest.java
@@ -17,41 +17,41 @@ public class CalcTest
     @Test
     public void testAdding()
     {
-        Assert.assertEquals(3, calc.add(1,2));
-        Assert.assertEquals(0, calc.add(10,-10));
-        Assert.assertEquals(100, calc.add(0,100));
-        Assert.assertEquals(100000000, calc.add(100000001,-1));
-        Assert.assertEquals(0.5, calc.add(1,-0.5));
-        Assert.assertEquals(1.5, calc.add(1,0.5));
+        Assert.assertEquals(3, calc.add(1,2), 0.01);
+        Assert.assertEquals(0, calc.add(10,-10), 0.01);
+        Assert.assertEquals(100, calc.add(0,100), 0.01);
+        Assert.assertEquals(100000000, calc.add(100000001,-1), 0.01);
+        Assert.assertEquals(0.5, calc.add(1,-0.5), 0.01);
+        Assert.assertEquals(1.5, calc.add(1,0.5), 0.01);
     }
 
     @Test
     public void testSubtract()
     {
-        Assert.assertEquals(-1, calc.subtract(1,2));
-        Assert.assertEquals(20, calc.subtract(10,-10));
-        Assert.assertEquals(-100, calc.subtract(0,100));
-        Assert.assertEquals(100000002, calc.subtract(100000001,-1));
-        Assert.assertEquals(1.5, calc.subtract(1,-0.5));
-        Assert.assertEquals(0.5, calc.subtract(1,0.5));
+        Assert.assertEquals(-1, calc.subtract(1,2), 0.01);
+        Assert.assertEquals(20, calc.subtract(10,-10), 0.01);
+        Assert.assertEquals(-100, calc.subtract(0,100), 0.01);
+        Assert.assertEquals(100000002, calc.subtract(100000001,-1), 0.01);
+        Assert.assertEquals(1.5, calc.subtract(1,-0.5),0.01);
+        Assert.assertEquals(0.5, calc.subtract(1,0.5),0.01);
     }
 
     @Test
     public void testMultiply()
     {
-        Assert.assertEquals(2, calc.multiply(1,2));
-        Assert.assertEquals(-100, calc.multiply(10,-10));
-        Assert.assertEquals(0, calc.multiply(0,100));
-        Assert.assertEquals(-100000001, calc.multiply(100000001,-1));
-        Assert.assertEquals(-0.5, calc.multiply(1,-0.5));
-        Assert.assertEquals(0.5, calc.multiply(1,0.5));
+        Assert.assertEquals(2, calc.multiply(1,2),0.01);
+        Assert.assertEquals(-100, calc.multiply(10,-10),0.01);
+        Assert.assertEquals(0, calc.multiply(0,100),0.01);
+        Assert.assertEquals(-100000001, calc.multiply(100000001,-1),0.01);
+        Assert.assertEquals(-0.5, calc.multiply(1,-0.5),0.01);
+        Assert.assertEquals(0.5, calc.multiply(1,0.5),0.01);
     }
 
     @Test
     public void testDivide()
     {
-        Assert.assertEquals(2, calc.divide(4,2));
-        Assert.assertEquals(1, calc.divide(10,10));
+        Assert.assertEquals(2, calc.divide(4,2),0.01);
+        Assert.assertEquals(1, calc.divide(10,10), 0.01);
     }
     @Test(expected = java.lang.ArithmeticException.class)
     public void testDivideByZero()
